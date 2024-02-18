621ea847c36672d9f7ae3fb105fa8c974b0b30c3
CatDuck <129601174+catduckgnaf@users.noreply.github.com>
2024-02-17 21:40:32 -0500

    Update media_player.py

--- custom_components/yamaha_soundbar/media_player.py
+++ custom_components/yamaha_soundbar/media_player.py
@@ -2,7 +2,7 @@
 Support for Yamaha Linkplay A118 based devices.
 
 For more details about this platform, please refer to the documentation at
-https://github.com/catduckgnaf/yamaha_soundbar
+https://github.com/catduckgnaf/yamaha-soundbar
 """
 
 import asyncio
@@ -44,8 +44,9 @@ from homeassistant.components.media_player import (
     PLATFORM_SCHEMA,
     MediaPlayerEntity,
     MediaPlayerEntityFeature,
-    MediaPlayerDeviceClass,
     MediaPlayerState,
+    MediaType,
+    MediaPlayerDeviceClass,
     BrowseMedia,
 )
 
@@ -109,7 +110,7 @@ ICON_PUSHSTREAM = 'mdi:cast-audio'
 ICON_TTS = 'mdi:text-to-speech'
 
 ATTR_SLAVE = 'slave'
-ATTR_LINKPLAY_GROUP = 'yamaha_group'
+ATTR_YAMAHA_GROUP = 'yamaha_group'
 ATTR_FWVER = 'firmware'
 ATTR_TRCNT = 'tracks_local'
 ATTR_TRCRT = 'track_current'
@@ -121,6 +122,7 @@ ATTR_DEBUG = 'debug_info'
 ATTR_MASS_POSITION = 'media_position_mass'
 
 CONF_NAME = 'name'
+CONF_SOURCE_IGNORE = "source_ignore"
 CONF_LASTFM_API_KEY = 'lastfm_api_key'
 CONF_SOURCES = 'sources'
 CONF_COMMONSOURCES = 'common_sources'
@@ -209,6 +211,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_COMMONSOURCES): cv.ensure_list,
         vol.Optional(CONF_LASTFM_API_KEY): cv.string,
         vol.Optional(CONF_UUID, default=''): cv.string,
+        vol.Optional(CONF_SOURCE_IGNORE, default=[]): vol.All( cv.ensure_list, [cv.string]),
         vol.Optional(CONF_VOLUME_STEP, default=DEFAULT_VOLUME_STEP): vol.All(int, vol.Range(min=1, max=25)),
         vol.Optional(CONF_ANNOUNCE_VOLUME_INCREASE, default=DEFAULT_ANNOUNCE_VOLUME_INCREASE): vol.All(int, vol.Range(min=0, max=50)),
     }
@@ -1004,8 +1007,6 @@ class YamahaDevice(MediaPlayerEntity):
 
         return self._features
 
-    SUPPORTED_COMMANDS = MediaPlayerEntityFeature.TURN_ON | MediaPlayerEntityFeature.TURN_OFF
-
     @property
     def media_position(self):
         """Time in seconds of current playback head position."""
@@ -1109,7 +1110,7 @@ class YamahaDevice(MediaPlayerEntity):
         """List members in group and set master and slave state."""
         attributes = {}
         if self._multiroom_group != []:
-            attributes[ATTR_LINKPLAY_GROUP] = self._multiroom_group
+            attributes[ATTR_YAMAHA_GROUP] = self._multiroom_group
             attributes[ATTR_GROUP_MEMBERS] = self._multiroom_group
 
         attributes[ATTR_MASTER] = self._is_master
@@ -1647,40 +1648,43 @@ class YamahaDevice(MediaPlayerEntity):
         if value == "OK":
             self._muted = bool(int(mute))
 
-    async def  async_turn_on(self):
-        """Turn the soundbar on."""
-        # Add the actual implementation to turn on the media player here
-        pass
+    async def async_turn_on(self):
+        """Use Mune/Unmute instead, because power is not supported."""
+        await self.async_mute_volume(False)
 
     async def async_turn_off(self):
-        """Turn the soundbar off."""
-        # Add the actual implementation to turn off the media player here
-        pass
+        """Use Mune/Unmute instead, because power is not supported."""
+        await self.async_mute_volume(True)
 
-async def call_update_lastfm(self, cmd, params):
-    """Update LastFM metadata."""
-    url = "{0}{1}&{2}&api_key={3}&format=json".format(LASTFM_API_BASE, cmd, params, self._lastfm_api_key)
-    #_LOGGER.debug("Updating LastFM from URL: %s", url)
+    async def async_toggle(self):
+        """Use Mune/Unmute instead, because power is not supported."""
+        await self.async_mute_volume(not self._muted)
 
-    try:
-        websession = async_get_clientsession(self.hass)
-        response = await websession.get(url)
-        if response.status == HTTPStatus.OK:
-            data = await response.json(content_type=None) #response.text()
-        else:
+    async def call_update_lastfm(self, cmd, params):
+        """Update LastFM metadata."""
+        url = "{0}{1}&{2}&api_key={3}&format=json".format(LASTFM_API_BASE, cmd, params, self._lastfm_api_key)
+        #_LOGGER.debug("Updating LastFM from URL: %s", url)
+
+        try:
+            websession = async_get_clientsession(self.hass)
+            response = await websession.get(url)
+            if response.status == HTTPStatus.OK:
+                data = await response.json(content_type=None) #response.text()
+
+            else:
+                _LOGGER.error(
+                    "Get failed, response code: %s Full message: %s",
+                    response.status,
+                    response,
+                )
+                return False
+
+        except (asyncio.TimeoutError, aiohttp.ClientError) as error:
             _LOGGER.error(
-                "Get failed, response code: %s Full message: %s",
-                response.status,
-                response,
+                "Failed communicating with LastFM '%s': %s", self._name, type(error)
             )
             return False
-
-    except (asyncio.TimeoutError, aiohttp.ClientError) as error:
-        _LOGGER.error(
-            "Failed communicating with LastFM '%s': %s", self._name, type(error)
-        )
-        return False
-    return data
+        return data
 
     @Throttle(LFM_THROTTLE)
     async def async_get_lastfm_coverart(self):
